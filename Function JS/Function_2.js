689f5614670769c7a577384b93fdc70d4feffa00
Lea Vaiana <leavaiana@gmail.com>
2018-11-23 11:39:04 +0100

    Function isEven & factorial

---
+++ Function JS/Function_2.js
@@ -0,0 +1,31 @@
+//SCHEMA FUNZIONE
+//function doSomething(){
+//    code;
+//}
+
+
+
+// Funzione che seleziona solo i numeri pari, function isEven
+function isEven(num){ //num è l argomento dove inserire il valore
+    if(num%2 ===0){ //se il modulo del valore è zero, allora è un numero pari
+        return true
+    }else{          //otherwise
+        return false
+    }
+}
+
+//Essendo un booleano possiamo scrivere la funzione piú semplicemente:
+function isEven(num){
+	return num % 2 === 0;
+}
+
+
+  //definisco la funzione factorial
+  function factorial(num){
+    var result = 1; //definisco la variabile risultato
+    for(var i = 2; i <= num; i++){ //loop a partire dal 2, dato che 1*1 non avrebbe senso (factorial(4) 4 x 3 x 2 x 1)
+        result *= i; //conservo il risultatto dentro la variabile result
+    }
+    return result; //ottengo il risultato della variabile
+  }
+
